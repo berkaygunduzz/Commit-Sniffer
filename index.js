7dbdad893005cf266a69b3f5509e967911c8f1dc
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2024-05-01 15:10:25 +0300

    feat(LINGUIST-12): Added review

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Config } from "./const/config.js";
+import { publishPrReview } from "./request/request.js";
 import { process_pr } from "./utils/process_pr.js";
 /**
  * This is the main entrypoint to your Probot app
@@ -11,6 +12,7 @@ export default (app) => {
     app.log.info("CommitSniffer is loaded!");
 
     // console.log(getMethodLengths());
+    // publishPrReview("CommitSniffer", "literate-fortnight", 11, null);
 
     app.on("issues.opened", async (context) => {
         const issueComment = context.issue({
@@ -21,39 +23,70 @@ export default (app) => {
         return;
     });
 
-    app.on(["pull_request.opened", "pull_request.edited"], async (context) => {
-        // Process PR and check for code smells
-        process_pr(context)
-            .then((result) => {
-                // Create a comment TODO this part should be updated so that it becomes compatible with other fearutes
-                result.forEach((msgGroup) => {
-                    if (msgGroup.length == 0) {
-                        return;
-                    }
+    app.on(
+        [
+            "pull_request.opened",
+            "pull_request.synchronize",
+            "pull_request.edited", // edited action should be removed, it is only for test purposes
+        ],
+        async (context) => {
+            // Process PR and check for code smells
+            process_pr(context)
+                .then((result) => {
+                    // Create a comment TODO this part should be updated so that it becomes compatible with other fearutes
+                    result.forEach((msgGroup) => {
+                        if (msgGroup.length == 0) {
+                            return;
+                        }
 
-                    let msg = msgGroup;
-                    if (Array.isArray(msgGroup)) {
-                        msg = msgGroup.join("\n");
-                    } else {
-                    }
+                        let msg = msgGroup;
+                        if (Array.isArray(msgGroup)) {
+                            msg = msgGroup.join("\n");
+                        } else {
+                        }
 
-                    createComment(context, msg);
+                        // createComment(context, msg); // uncomment this
+                        createReview(context, "");
+                    });
+                })
+                .catch((error) => {
+                    console.error("Error processing PR:", error);
+                    const prComment = context.issue({
+                        body: "We cannot process your PR right now :(\nPlease try again later.",
+                    });
+                    return context.octokit.issues.createComment(prComment);
                 });
-            })
-            .catch((error) => {
-                console.error("Error processing PR:", error);
-                const prComment = context.issue({
-                    body: "We cannot process your PR right now :(\nPlease try again later.",
-                });
-                return context.octokit.issues.createComment(prComment);
-            });
-    });
+        }
+    );
 
     function createComment(context, msg) {
         const comment = context.issue({
             body: msg,
         });
         return context.octokit.issues.createComment(comment);
+    }
+
+    function createReview(context, msg) {
+        const owner = context.payload.repository.owner.login;
+        const repo = context.payload.repository.name;
+        const pull_number = context.payload.number;
+
+        const review = context.octokit.pulls.createReview({
+            owner: owner,
+            repo: repo,
+            pull_number: pull_number,
+            body: "Please update your branch according to suggested changes!",
+            event: "REQUEST_CHANGES", // Specify the review action (APPROVE, REQUEST_CHANGES, or COMMENT)
+            comments: [
+                {
+                    position: 6,
+                    path: "src/Client.java", // Path to the file you want to comment on
+                    body: "Consider simplifying the expression `if (isAvailable === false)` to `if (isAvailable)`. The variable `isAvailable` is already a boolean value, so there's no need for the explicit comparison to `false`. This simplification enhances readability and adheres to best practices for concise code.",
+                    // You can optionally specify comments[].position if needed
+                },
+                // Add more comments if necessary
+            ],
+        });
     }
 
     // For more information on building apps:
--- request/request.js
+++ request/request.js
@@ -5,6 +5,32 @@ const octokit = new Octokit({
     auth: Config.GITHUB_API_KEY,
 });
 
+export async function sendDiffFileRequest(owner, repo, pull_number) {
+    const url = "/repos/{owner}/{repo}/pulls/{pull_number}";
+    const acceptMediaTypes = [
+        "application/vnd.github.diff",
+        "application/vnd.github.raw+json",
+    ];
+
+    try {
+        const response = await octokit.request(`GET ${url}`, {
+            owner: owner,
+            repo: repo,
+            pull_number: pull_number,
+            headers: {
+                "X-GitHub-Api-Version": "2022-11-28",
+                Accept: acceptMediaTypes.join(","),
+            },
+        });
+
+        console.log("data:", response.data);
+
+        return response.data;
+    } catch (error) {
+        console.error("Error occurred while fetching the diff file:", error);
+    }
+}
+
 export async function sendFileRequest(owner, repo, pull_number) {
     const url = "/repos/{owner}/{repo}/pulls/{pull_number}/files";
 
@@ -17,6 +43,38 @@ export async function sendFileRequest(owner, repo, pull_number) {
                 "X-GitHub-Api-Version": "2022-11-28",
             },
         });
+
+        console.log("data:", response.data);
+
+        return response.data;
+    } catch (error) {
+        console.error("Error occurred while fetching files:", error);
+    }
+}
+
+export async function publishPrReview(owner, repo, pull_number, diff_file) {
+    const url = "/repos/{owner}/{repo}/pulls/{pull_number}/reviews";
+
+    try {
+        const response = await octokit.request(`POST ${url}`, {
+            owner: owner,
+            repo: repo,
+            pull_number: pull_number,
+            body: "Please update your branch according to suggested changes!",
+            event: "REQUEST_CHANGES",
+            comments: [
+                {
+                    path: "src/Client.java",
+                    position: 1,
+                    body: "Please add more information here, and fix this typo.",
+                },
+            ],
+            headers: {
+                "X-GitHub-Api-Version": "2022-11-28",
+            },
+        });
+
+        console.log("data:", response.data);
 
         return response.data;
     } catch (error) {
