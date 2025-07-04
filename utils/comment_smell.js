d8d1d47f8fa5da1397ebdafc428a562ce676f26c
Berkay Gündüz <70534725+berkaygunduzz@users.noreply.github.com>
2025-07-04 18:22:26 +0300

    Update comment_smell.js

--- utils/comment_smell.js
+++ utils/comment_smell.js
@@ -1,7 +1,7 @@
 import { multiPromptChat } from "./gemini_api.js";
 
 export async function checkCommentSmells(fileContent, filePath) {
-    const role = "You are a code comment reviewer. I will provide you a data in the format: array[string] which is an array of strings of code comments and the full code of the file. You will review those comments as 'expressive', that is, the comment is a useful for a codebase they can be even oneline codes explaining a variable, all comments related to the code is expressive, or 'non-expressive', that is, the comment adds no significant value to the codebase, they are not related to code, can be commented code pieces or author/license info also. Your output will be in the form like a JSON compatible string, in the format: \"{indexOfTheComment: 1 for expressive, 0 for non-expressive}\"";
+    const role = "You are a code comment reviewer. Your format: array[string] which is an array of strings of code comments and the full code of the file. You will review those comments as 'expressive', that is, the comment is a useful for a codebase they can be even oneline codes explaining a variable, all comments related to the code is expressive, or 'non-expressive', that is, the comment adds no significant value to the codebase, they are not related to code, can be commented code pieces or author/license info also. Your output will be in the form like a JSON compatible string, in the format: \"{indexOfTheComment: 1 for expressive, 0 for non-expressive}\"";
     const comments = extractComments(fileContent);
     if (comments.length > 0) {
         const prompt = `Comments: ${JSON.stringify(comments)}\nFull code: ${fileContent}`;
